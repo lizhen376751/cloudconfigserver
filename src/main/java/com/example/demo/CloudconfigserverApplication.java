7e70918201a644bd4195792fbe739f08f17d6c34
lizhen <724577807@qq.com>
2018-04-11 15:33:35 +0800

    高可用实现配置中心（集群）---完整版

--- src/main/java/com/example/demo/CloudconfigserverApplication.java
+++ src/main/java/com/example/demo/CloudconfigserverApplication.java
@@ -3,11 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @EnableConfigServer注解开启配置服务器的功能
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class CloudconfigserverApplication {
 
